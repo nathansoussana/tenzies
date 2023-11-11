65cdc96867be2bd10ce60b3da184fc462f4d50c1
Nathan Soussana <nathan.soussana@gmail.com>
2023-11-11 18:47:56 +0100

    Created die component and dice container

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,7 +1,20 @@
+import Die from './components/Die'
+
 function App() {
   return (
     <main>
-
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
     </main>
   )
 }
---
+++ src/jsx/components/Die.jsx
@@ -0,0 +1,5 @@
+export default function Die({ value }) {
+  return (
+    <div className="die">{value}</div>
+  )
+}
