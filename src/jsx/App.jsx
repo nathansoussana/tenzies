da3532824f55fd6d4cc35811d9010162f63b7c48
Nathan Soussana <nathan.soussana@gmail.com>
2023-11-12 14:32:44 +0100

    Changed each die to be an object so that they have a property called isHeld that will determine whether a button is held or not

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -6,22 +6,22 @@ export default function App() {
   const [dice, setDice] = useState(allNewDice())
 
   function allNewDice() {
-    const dice = Array.from({ length: 10 }, (die) => Math.ceil(Math.random(die) * 6))
+    const dice = Array.from({ length: 10 }, (die) => ({ value: Math.ceil(Math.random(die) * 6), isHeld: false }))
     return dice
   }
 
-  function handleClick() {
+  function rollDice() {
     setDice(allNewDice())
   }
 
-  const diceHand = dice.map((die, index) => <Die key={index} value={die} />)
+  const diceHand = dice.map((die, index) => <Die key={index} value={die.value} />)
 
   return (
     <main>
       <div className="dice-container">
         {diceHand}
       </div>
-      <Button text="Roll" onClick={handleClick} />
+      <Button text="Roll" onClick={rollDice} />
     </main>
   )
 }
